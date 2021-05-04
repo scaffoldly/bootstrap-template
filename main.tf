fad2a7bebba41ee6ad2add82db02b2eb66e773ac
Christian Nuss <cnuss@users.noreply.github.com>
2021-05-04 11:58:30 -0700

    scaffoldly/bootstrap/scaffoldly 0.15.18

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 module "bootstrap" {
   source  = "scaffoldly/bootstrap/scaffoldly"
-  version = "0.15.14"
+  version = "0.15.18"
 
   root_email   = var.ROOT_EMAIL
   github_token = var.BOOTSTRAP_GITHUB_TOKEN
